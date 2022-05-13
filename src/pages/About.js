9ba0933e206724bafbe3104b3f3d3ec64e7d4362
kimochiee <90102971+kimochiee@users.noreply.github.com>
2022-05-13 17:58:37 +0700

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>The paper was blank. It shouldn't have been. There should have been writing on the paper, at least a paragraph if not more. The fact that the writing wasn't there was frustrating. Actually, it was even more than frustrating. It was downright distressing.</p>
+    </section>
+  )
+}
+
+export default About
